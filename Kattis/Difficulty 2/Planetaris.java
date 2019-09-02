71540632d780f8c6b282ef57eeafe697c591ece6
Edward Flores <EdwardVFlores@gmail.com>
2019-09-01 20:24:24 -1000

    2.1 Difficulty

--- Kattis/Difficulty 2/Planetaris.java
+++ Kattis/Difficulty 2/Planetaris.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-
-/*
-*
-* @author Edward Flores
-* Link: https://open.kattis.com/problems/planetaris
-*/
-public class Planetaris {
+package programmingchallenges;
+import java.util.*;
+/**
+ * @author Edward Flores
+ * Link: https://open.kattis.com/problems/planetaris
+ */
+ 
+public class planetaris {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int numSystems = keyboard.nextInt(), numShips = keyboard.nextInt(), wins = 0;
         ArrayList<Integer> enemyShips = new ArrayList();
         for (int i = 0; i < numSystems; i++)
             enemyShips.add(keyboard.nextInt());
-
         Collections.sort(enemyShips);
-
         for (int element : enemyShips) {
             if (numShips > element) {
                 numShips -= (element + 1);
