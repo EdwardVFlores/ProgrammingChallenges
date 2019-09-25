1415475a116531f04cac3ad8524301c449ab33ab
Edward Flores <EdwardVFlores@gmail.com>
2019-09-24 20:36:55 -1000

    3.2 Difficulty

---
+++ Kattis/Difficulty 3/HIndex.java
@@ -0,0 +1,11 @@
+package ProgrammingChallenges;
+/**
+ * @author Edward Flores
+ * Link: https://open.kattis.com/problems/hindex
+ */
+public class HIndex {
+    public static void main(String[] args){
+        //Sorry I got the answer but this code put me on the leaderboard
+        //So I have decided to not post it :)
+    }
+}
