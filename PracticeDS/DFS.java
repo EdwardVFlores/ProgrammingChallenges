e6e10c7e0bf2c6b3aeab8b28816feaa939054434
EdwardVFlores <EdwardVFlores@gmail.com>
2021-08-14 00:03:57 -1000

    BFS practice

--- PracticeDS/DFS.java
+++ PracticeDS/DFS.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 class DFS {
 
     static class Node{
@@ -17,11 +17,18 @@ class DFS {
         Node root = new Node(5, 
         new Node(10, new Node(20), new Node(8)), 
         new Node(7, new Node(30), new Node(14)));
-        ArrayList<Node> res = new ArrayList<>();
-        DFS(root,res);
-        for(Node n : res){
+        ArrayList<Node> dfsRes = new ArrayList<>();
+        ArrayList<Node> bfsRes = new ArrayList<>();
+        DFS(root,dfsRes);
+        for(Node n : dfsRes){
             System.out.println(n.val);
         }
+        System.out.println("");
+        BFS(root,bfsRes);
+        for(Node n : bfsRes){
+            System.out.println(n.val);
+        }
+
     } 
 
     static void DFS(Node root, ArrayList<Node> res){
@@ -29,5 +36,17 @@ class DFS {
         if(root.left != null) DFS(root.left, res);
         res.add(root);
         if(root.right != null) DFS(root.right,res);
+    }
+
+    static void BFS(Node root, ArrayList<Node>res){
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node cur = q.remove();
+            res.add(cur);
+            if(cur.left != null) q.add(cur.left);
+            if(cur.right != null) q.add(cur.right);
+            
+        }
     }
 }
