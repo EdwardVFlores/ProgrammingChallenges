b547ffa8193b75fe39c235d838ccaec814bbd885
Edward Flores <edwardvflores@gmail.com>
2022-02-05 17:59:18 -1000

    Post order dfs traversal

--- LeetCode/Easy Questions/BinaryTreePostorderTraversal.java
+++ LeetCode/Easy Questions/BinaryTreePostorderTraversal.java
@@ -1,10 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    List<Integer> res = new ArrayList<>();
-    public List<Integer> postorderTraversal(TreeNode root) {
-        if(root == null) return res;
-        if(root.left != null) postorderTraversal(root.left);
-        if(root.right != null) postorderTraversal(root.right);
-        res.add(root.val);
-        return res;
+    List<Integer> answer = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return answer;
+        inorderTraversal(root.left);
+        answer.add(root.val);
+        inorderTraversal(root.right);
+        return answer;
     }
 }
